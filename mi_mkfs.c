9de7cc08e712a71d6def58d146fa62b98d3cb82f
ehe781 <emanuel.hegedus18@gmail.com>
2020-02-20 22:22:39 +0100

    Parte 1 completada

--- mi_mkfs.c
+++ mi_mkfs.c
@@ -1,14 +1,16 @@
 #include "bloques.h"
-char cwd[PATH_MAX];
+const char *cwd;
 int nbloques = 0;
-unsigned char *buf;
+unsigned char *buf[BLOCKSIZE];
 
 int main(int argc, char **argv){
     memset(buf, 0, BLOCKSIZE);
-    bmount("/home/ehe781/SOII_SF/disco");
+    cwd = argv[1];
+    bmount(cwd);
     nbloques = atoi(argv[2]);
     for(int i = 0 ; i < nbloques ; i++){
         bwrite(i, buf);
     }
     bumount();
+    return 0;
 }
