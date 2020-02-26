5d3098f1720c3821608fdd41cf7b7b94180d727c
EHE781 <56025935+EHE781@users.noreply.github.com>
2020-02-26 01:04:21 +0100

    PARTE 2 completada

--- PARTE 1/mi_mkfs.c
+++ PARTE 1/mi_mkfs.c
@@ -1,4 +1,8 @@
 #include "bloques.h"
+<<<<<<< HEAD
+#include "ficheros_basico.h"
+=======
+>>>>>>> 93f611ccc4fa8532ea6816c1bc67985a015665fe
 const char *cwd;
 int nbloques = 0;
 unsigned char *buf[BLOCKSIZE];
@@ -11,6 +15,9 @@ int main(int argc, char **argv){
     for(int i = 0 ; i < nbloques ; i++){
         bwrite(i, buf);
     }
+    initSB(nbloques, nbloques/4); //para cada bloque un bloque y para cada bloque un inodo
+    initMB();
+    initAI();
     bumount();
     return 0;
 }
