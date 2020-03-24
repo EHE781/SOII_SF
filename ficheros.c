616aac25bd5d291242cce669c7edd9f2aa5f4ab9
BCS096 <tomeucs99@gmail.com>
2020-03-24 16:10:44 +0100

    cosas

--- ficheros.c
+++ ficheros.c
@@ -1,6 +1,6 @@
 int mi_write_f(unsigned int ninodo, const void *buf_original,unsigned int offset ,unsigned int nbytes){
     void *buf_bloque;
-    if(inodo.permisos & 2){
+    if(inodo.permisos & 2==2){
         int primerBloqueLog=offset/BLOCKSIZE;
         int ultimoBloqueLog=(offset+nbytes-1)/BLOCKSIZE;
         int desp1=offset%BLOCKSIZE;
