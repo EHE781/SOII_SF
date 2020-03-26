512df980bafdb96c0396cd4dc2d14b87c1ca1ab6
BCS096 <tomeucs99@gmail.com>
2020-03-26 17:33:56 +0100

    .

--- ficheros.h
+++ ficheros.h
@@ -1,3 +1,4 @@
 #include "ficheros_basico.h"
 
 int mi_write_f(unsigned int ninodo, const void *buf_original,unsigned int offset ,unsigned int nbytes);
+int mi_read_f(unsigned int ninodo, void *buf_original,unsigned int offset,unsigned int nbytes);
