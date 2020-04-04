7836a94dd817293980d74b4aa4dfa317b750ae94
ehe781 <emanuel.hegedus18@gmail.com>
2020-04-04 18:55:33 +0200

    fixed

--- escribir.c
+++ escribir.c
@@ -13,27 +13,43 @@ int main(int argc, char **argv){
     int ninodo;
     const char *dir;
     unsigned int length;
-    int diferentes_inodos;
+    length = strlen(argv[2]);
+    char *buf = malloc(length);
+    int diferentes_inodos,nBytes,nbytes,inodos;
+    int offset[4] = {9000, 209000, 30725000, 409605000};
     if(argv[1] == NULL || argv[2] == NULL || argv[2] == NULL || argv[3] == NULL){
         fprintf(stderr,"La sintaxi correcta es: escribir <nombre_dispositivo> <\"$(cat fichero)\"> <diferentes_inodos>\n");
         return EXIT_FAILURE;
     }
     dir = argv[1];
+    inodos = atoi(argv[3]);
     bmount(dir);
     bread(posSB, &SB);
+    if(inodos == 0){
     ninodo = reservar_inodo('f', 6);
-    printf("El ninodo es: %i\n", ninodo);
-    length = strlen(argv[2]);
-    char *buf = malloc(length);
-    strcpy(buf, argv[2]);
-    int nBytes = mi_write_f(ninodo, buf, 9000, length);
-    memset(buf, 0, length);
-    int nbytes = mi_read_f(ninodo, buf, 9000, length);
-    printf("EL texto es: ");
-    for(int i = 0; i<length; i++){
-            printf("%c", buf[i]);
+    for(int i = 0; i < 4; i++){
+        printf("El ninodo es: %i\n", ninodo);
+        strcpy(buf, argv[2]);
+        nBytes += mi_write_f(ninodo, buf, offset[i], length);
+        memset(buf, 0, length);
+        nbytes += mi_read_f(ninodo, buf, offset[i], length);
+        write(1, buf, length);
+      }
     }
-    printf("\n");
+    else if(inodos == 1){
+      for(int i = 0; i < 4; i++){
+        ninodo = reservar_inodo('f', 6);
+        printf("El ninodo es: %i\n", ninodo);
+        strcpy(buf, argv[2]);
+        nBytes += mi_write_f(ninodo, buf, offset[i], length);
+        memset(buf, 0, length);
+        nbytes += mi_read_f(ninodo, buf, offset[i], length);
+        write(1, buf, length);
+      }
+    }
+    else{
+      printf("No es una opción de inodo válida, %s", inodos);
+    }
     bumount(dir);
     return EXIT_SUCCESS;
 }
