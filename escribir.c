966e6adcec2a127f0b542fe37cc710fd9abdd73b
ehe781 <emanuel.hegedus18@gmail.com>
2020-04-04 17:56:02 +0200

    hecho escribir.c

--- escribir.c
+++ escribir.c
@@ -9,18 +9,31 @@
 4.Pasar como argumento el nombre de un fichero externo que contenga el texto
 */
 int main(int argc, char **argv){
-    void *buf;
-    if(argv[1] == NULL){
-        fprintf(stderr,"La sintaxi correcta es \'-t \"texto\"\' o bien \'directorio\'\n");
+    struct superbloque SB;
+    int ninodo;
+    const char *dir;
+    unsigned int length;
+    int diferentes_inodos;
+    if(argv[1] == NULL || argv[2] == NULL || argv[2] == NULL || argv[3] == NULL){
+        fprintf(stderr,"La sintaxi correcta es: escribir <nombre_dispositivo> <\"$(cat fichero)\"> <diferentes_inodos>\n");
         return EXIT_FAILURE;
     }
-    buf = argv[1];
-    if(argv[1] == buf){
-        if(argv[2] == NULL){
-            fprintf(stderr,"La sintaxi correcta es \'-t \"texto\"\' o bien \'directorio\'\n");
-            return EXIT_FAILURE;
-        }
-        buf = argv[2];
+    dir = argv[1];
+    bmount(dir);
+    bread(posSB, &SB);
+    ninodo = reservar_inodo('f', 6);
+    printf("El ninodo es: %i\n", ninodo);
+    length = strlen(argv[2]);
+    char *buf = malloc(length);
+    strcpy(buf, argv[2]);
+    int nBytes = mi_write_f(ninodo, buf, 9000, length);
+    memset(buf, 0, length);
+    int nbytes = mi_read_f(ninodo, buf, 9000, length);
+    printf("EL texto es: ");
+    for(int i = 0; i<length; i++){
+            printf("%c", buf[i]);
     }
+    printf("\n");
+    bumount(dir);
     return EXIT_SUCCESS;
 }
--- ficheros.c
+++ ficheros.c
@@ -1,30 +1,50 @@
 #include "ficheros.h"
 int mi_write_f(unsigned int ninodo, const void *buf_original,unsigned int offset ,unsigned int nbytes){
     struct inodo inodo;
-    char buf_bloque[BLOCKSIZE];
-    leer_inodo(ninodo,&inodo);
-    if(inodo.permisos & 2 == 2){
+    unsigned char *buf_bloque[BLOCKSIZE];
+    int total;
+    leer_inodo(ninodo, &inodo);
+    if((inodo.permisos & 2) == 2){
         int primerBLogico = offset/BLOCKSIZE;
         int ultimoBLogico = (offset + nbytes - 1) / BLOCKSIZE;
         int desp1 = offset % BLOCKSIZE;
-        int desp2 = ultimoBLogico % BLOCKSIZE;
+        int desp2 = (offset + nbytes) % BLOCKSIZE;
         int BFisico = traducir_bloque_inodo(ninodo,primerBLogico,1);
         if(desp1 != 0){
             bread(BFisico, buf_bloque);
         }
+        if(primerBLogico == ultimoBLogico){
+            memcpy(buf_bloque + desp1, buf_original, desp2 + 1 - desp1);//-desp1...
+            total += bwrite(BFisico, buf_bloque);
+        }
+        else{
+            desp2 = offset + nbytes - 1;
+            memcpy (buf_bloque + desp1, buf_original, BLOCKSIZE - desp1);
+            total += bwrite(BFisico, buf_bloque);
+            for(int i = primerBLogico + 1; i != ultimoBLogico; i ++){
+                BFisico = traducir_bloque_inodo(ninodo, i, 1);
+                total += bwrite(BFisico, (buf_original + (BLOCKSIZE - desp1) + (i - primerBLogico - 1) * BLOCKSIZE));
+            }
+            desp2 = desp2 % BLOCKSIZE;
+            BFisico = traducir_bloque_inodo(ninodo, ultimoBLogico, 1);
+            bread(BFisico, buf_bloque);
+            memcpy (buf_bloque, buf_original + (nbytes - desp2 - 1), desp2 + 1);
+            total += bwrite(BFisico, buf_bloque);
+        }
+        /*Antiguo codigo
         memcpy(buf_bloque + desp1,buf_original,BLOCKSIZE - desp1);
-        int total=bwrite(BFisico,buf_bloque);
+        memcpy(buf_bloque + desp1,buf_original,desp2 - desp1 + 1);
+        int total = bwrite(BFisico, buf_bloque);
         for(int i = primerBLogico + 1;i != ultimoBLogico;i++){
-            total = total + bwrite(traducir_bloque_inodo(ninodo,i,1),
-            buf_original + (BLOCKSIZE - desp1) + (i - primerBLogico - 1) * BLOCKSIZE);
+            memcpy(aux, (buf_original + (BLOCKSIZE - desp1) + (i - primerBLogico - 1) * BLOCKSIZE), BLOCKSIZE);
+            total += bwrite(traducir_bloque_inodo(ninodo, i, 1), aux);
         }
         int ultimoBFisico = traducir_bloque_inodo(ninodo,ultimoBLogico,1);
         if(desp2!=BLOCKSIZE){
             bread(ultimoBFisico,buf_bloque);
         }
         memcpy(buf_bloque,buf_original + (nbytes-desp2 - 1),desp2 + 1);
-        total = total + bwrite(ultimoBFisico, buf_bloque);
-        leer_inodo(ninodo,&inodo);
+        total = total + bwrite(ultimoBFisico, buf_bloque);*/
         if(inodo.tamEnBytesLog < total){//modifica EOF fichero
             inodo.tamEnBytesLog = total; //Si lo escrito ocupa más que lo que ya habia
             inodo.ctime = time(NULL);// Actualizamos ctime, hemos modificado el inodo
@@ -41,13 +61,38 @@ int mi_write_f(unsigned int ninodo, const void *buf_original,unsigned int offset
 int mi_read_f(unsigned int ninodo, void *buf_original,unsigned int offset,unsigned int nbytes){
     struct inodo inodo;
     leer_inodo(ninodo, &inodo);
-    char buf_bloque[BLOCKSIZE];
-    if (inodo.permisos & 4 == 4){
-        int bytesLeidos=0;
+    unsigned char *buf_bloque[BLOCKSIZE];
+    if ((inodo.permisos & 4) == 4){
+        int bytesLeidos = 0;
         int primerBLogico = offset/BLOCKSIZE;
         int ultimoBLogico = (offset + nbytes - 1) / BLOCKSIZE;
         int desp1 = offset % BLOCKSIZE;
-        int desp2 = ultimoBLogico % BLOCKSIZE;
+        int desp2 = (offset + nbytes) % BLOCKSIZE;
+        int BFisico = traducir_bloque_inodo(ninodo, primerBLogico, 0);
+        if(primerBLogico == ultimoBLogico){
+            bytesLeidos += bread(BFisico, buf_bloque);
+            memcpy(buf_original, buf_bloque + desp1, desp2 + 1 - desp1);
+        }
+        else{
+            //posible falta de deteccion de bloques vacíos error
+            desp2 = offset + nbytes - 1;
+            bytesLeidos += bread(BFisico, buf_bloque);
+            memcpy (buf_original, buf_bloque + desp1, BLOCKSIZE - desp1);
+            for(int i = primerBLogico + 1; i != ultimoBLogico; i ++){
+                BFisico = traducir_bloque_inodo(ninodo, i, 0);
+                if(BFisico != -1){
+                    bytesLeidos += bread(BFisico, buf_bloque);
+                    memcpy(buf_original, buf_bloque, BLOCKSIZE);
+                }else{
+                    bytesLeidos += 1; // bytes bloque vacío?!?
+                }
+            }
+            desp2 = desp2 % BLOCKSIZE;
+            BFisico = traducir_bloque_inodo(ninodo, ultimoBLogico, 0);
+            bytesLeidos += bread(BFisico, buf_bloque);
+            memcpy (buf_original, buf_bloque, desp2 + 1);
+        }
+        /*ANTIGUO CODIGO borrable? nose
         for (int i = primerBLogico; i != ultimoBLogico + 1; i++) { // != ultimoBLogico + 1(este +1 es para que me lea tmb el ultimo bloque)
             int BFisico = traducir_bloque_inodo(ninodo,i,0);
             bread(BFisico,buf_bloque);
@@ -64,7 +109,7 @@ int mi_read_f(unsigned int ninodo, void *buf_original,unsigned int offset,unsign
                 bytesLeidos = bytesLeidos + BLOCKSIZE;
             }
 
-        }
+        }*/
         inodo.atime = time(NULL);
         escribir_inodo(ninodo, inodo);
         return bytesLeidos;
