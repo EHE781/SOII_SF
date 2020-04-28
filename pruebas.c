969eddea1658f46c32418a9a88395dab7a3e1317
ehe781 <emanuel.hegedus18@gmail.com>
2020-04-28 13:44:36 +0200

    no va

--- pruebas.c
+++ pruebas.c
@@ -5,7 +5,8 @@ int main(int argc, char **argv){
     char *camino = malloc(10);
     camino = "/directorio/fichero.txt";
     char *inicial = malloc(10);
-    char *tipo = malloc(10);
-    tipo = 'd';
+    char *tipo = malloc(1);
+    tipo = 'a';
     int i = extraer_camino(camino, inicial, final, tipo);
+    fprintf(stderr, "Le hemos pasado un %c, y el return es %i\n", tipo, i);
 }
