f4cad5c90a87d3be35b34a62002100f994fb8a29
ehe781 <emanuel.hegedus18@gmail.com>
2020-04-28 13:19:20 +0200

    correccion

--- directorios.c
+++ directorios.c
@@ -8,12 +8,17 @@ int extraer_camino(const char *camino, char *inicial, char *final, char *tipo){
                 strncpy(inicial, camino, sizeof(char) * i - 1);
                 hayDir = true;
                 strcpy(final, camino + i);
+                tipo = 'd';
                 return 1;
             }
         }
         if(!hayDir){
             strncpy(inicial, camino + 1, sizeof(char) * strlen(camino) - 1);//+1 para evitar la primera '/'
+            tipo = 'f';
             return 0;
         }
+    }
+    else{
+        return EXIT_FAILURE;
     }
 }
