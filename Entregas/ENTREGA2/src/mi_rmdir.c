bcbf08f155cd1d52d55e583177df286dd514e7db
ehe781 <emanuel.hegedus18@gmail.com>
2020-05-18 13:53:56 +0200

    Entrega 2 y intento de mi_rn

---
+++ Entregas/ENTREGA2/src/mi_rmdir.c
@@ -0,0 +1,42 @@
+#include "headers/directorios.h"
+
+int main(int argc, char **argv)
+{
+    if (argc == 4)
+    {
+        if (argv[3][strlen(argv[3] - 1)] != '/')
+        {
+            fprintf(stderr, "No es un directorio váilido\n");
+            return EXIT_FAILURE;
+        }
+        else
+        {
+            if (strlen(argv[1]) != 2 || argv[1][1] != 'r')
+            {
+                fprintf(stderr, "Parámetro inválido, prueba con \"-r\"\n");
+                return EXIT_FAILURE;
+            }
+        }
+        bmount(argv[2]);
+        mi_unlink(argv[3], true); //preguntar si se puede booleana
+        bumount(argv[2]);
+        return EXIT_SUCCESS;
+    }
+    else
+    {
+        if (argc == 3)
+        {
+            if (argv[2][strlen(argv[2] - 1)] != '/')
+            {
+                fprintf(stderr, "No es un directorio váilido\n");
+                return EXIT_FAILURE;
+            }
+            bmount(argv[1]);
+            mi_unlink(argv[2], false); //preguntar si se puede booleana
+            bumount(argv[1]);
+            return EXIT_SUCCESS;
+        }
+    }
+    fprintf(stderr, "La sintaxis correcta es ./mi_rmdir [opcional-> -r] disco /ruta");
+    return EXIT_FAILURE;
+}
